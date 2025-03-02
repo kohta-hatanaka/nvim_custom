e7e9268a2499df911b6767c9ff9db6c62bff4a7a
kohta-hatanaka <htnk.duckbat6412@gmail.com>
2025-03-02 10:02:52 +0900

    250302:setup keymaps, copilot cmd

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set({ "i", "c" }, "jf", "<Esc>", { desc = "Escape with jf", noremap = true, silent = true })
+vim.keymap.set({ "i", "c" }, "fj", "<Esc>", { desc = "Escape with jf", noremap = true, silent = true })
+--vim.keymap.set(
+--  { "i", "v", "x", "s", "o", "c", "t" },
+--  "jj",
+--  "<Esc>",
+--  { desc = "Escape with jj", noremap = true, silent = true }
+--)
---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,56 @@
+return {
+  "hrsh7th/nvim-cmp",
+  enabled = true,
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-path",
+    "saadparwaiz1/cmp_luasnip",
+    "L3MON4D3/LuaSnip",
+    "zbirenbaum/copilot-cmp",
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
+      sources = cmp.config.sources({
+        { name = "copilot", priority = 1000 },
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+      }),
+    })
+  end,
+}
